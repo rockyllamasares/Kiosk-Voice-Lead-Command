9edb8f552330f6101fc112f7ecbecc9c5f394522
rockyllamasares <rockyllamasares08@gmail.com>
2023-02-05 01:45:34 +0800

    change index title to dashboard

--- admin/home.php
+++ admin/home.php
@@ -8,7 +8,7 @@ include_once("header.php");
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Index</h1>
+            <h1>Dashboard</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
