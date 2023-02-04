e1a2288dbfb4bee1706253e1d9eb4c81cef091df
rockyllamasares <rockyllamasares08@gmail.com>
2023-02-05 01:03:58 +0800

    delete header navigate home and contact and on report change order list to report

--- admin/report.php
+++ admin/report.php
@@ -28,12 +28,12 @@ include("header.php");
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Order List</h1>
+            <h1>Report</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="home.php">Home</a></li>
-              <li class="breadcrumb-item active">Order List</li>
+              <li class="breadcrumb-item active">Report</li>
             </ol>
           </div>
         </div>
